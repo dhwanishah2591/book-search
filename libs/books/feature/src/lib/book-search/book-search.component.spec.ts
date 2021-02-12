744ba52d97712db72f5510a2c58d8829a893dcb4
DShah16 <dhwani.shah16@t-mobile.com>
2021-02-12 14:24:26 +0530

    Task2: instant search

--- libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -1,13 +1,16 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedTestingModule } from '@tmo/shared/testing';
 
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
+import { Store } from '@ngrx/store';
 
 describe('BookSearchComponent', () => {
   let component: BookSearchComponent;
   let fixture: ComponentFixture<BookSearchComponent>;
+  let store: Store;
+  let storeDispatchSpy: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,9 +22,37 @@ describe('BookSearchComponent', () => {
     fixture = TestBed.createComponent(BookSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    jest.spyOn(component, 'searchBooks');
+    store = TestBed.inject(Store);
+    storeDispatchSpy = jest.spyOn(store, 'dispatch');
   });
 
   it('should create', () => {
     expect(component).toBeDefined();
   });
+
+  it('should not call searchBooks after 500ms for same input', fakeAsync(() => {
+    component.searchForm.controls['term'].setValue('javascript');
+    tick(500);
+    component.searchForm.controls['term'].setValue('javascript');
+    tick(500);
+    expect(component.searchBooks).toHaveBeenCalledTimes(1);
+    expect(storeDispatchSpy).toHaveBeenCalledTimes(1);
+  }));
+
+  it('should call searchBooks after 500ms for diffrent input', fakeAsync(() => {
+    component.searchForm.controls['term'].setValue('javascript');
+    tick(500);
+    component.searchForm.controls['term'].setValue('html');
+    tick(500);
+    expect(component.searchBooks).toHaveBeenCalledTimes(2);
+    expect(storeDispatchSpy).toHaveBeenCalledTimes(2);
+  }));
+
+  it('should not subscribe to input when component destroyed', fakeAsync(() => {
+    component.ngOnDestroy();
+    component.searchForm.controls['term'].setValue('java');
+    tick(500);
+    expect(component.searchBooks).not.toHaveBeenCalled();
+  }));
 });
